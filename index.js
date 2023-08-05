918c085ec6afc9b63a8102e1a1d2d830f91b5ed6
AguBustos <bustosagustin103@gmail.com>
2023-08-04 23:30:57 -0300

    segunda version

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ function hello_handler(req, res){
 res.send('bye');
 
 };
-
+function hello_handler2(req, res){
+    res.send('hola papus');
+    
+    };
+    
 app.get('/hello', hello_handler);
+app.get('/hola', hello_handler2);
 app.listen(port, function(){
     console.log(`server running on http://localhost:${port}`);
 });
