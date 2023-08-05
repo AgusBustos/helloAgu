7e3bba67c39c63ea07f4de0b1dbc837ba52a0062
AguBustos <bustosagustin103@gmail.com>
2023-08-05 11:58:24 -0300

    dia 2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 function hello_handler(req, res) {
     res.send('bye');
 
-};
+}
 
 function create_users(req, res) {
     console.log("Entre");
@@ -28,11 +28,30 @@ function hello_handler2(req, res) {
 function devolverUsarios(req, res){
     res.status(200).json(lista_de_usuarios);
 }
+function verificarUser(req, res){
+
+    const usuario = req.body.username;
+    const contra = req.body.password;
+    const usuarioencontrado = lista_de_usuarios.find(buscarUser);
+    function buscarUser(u){
+        if (u.username === usuario && u.password === contra){
+            return true;
+        } else{
+            return false;
+        }
+    }
+    if (usuarioencontrado === undefined){
+        res.status(401).send("no tiene permiso");
+    } else{
+        res.status(200).send("OK");
+    }
+}
 
 app.get('/hello', hello_handler);
 app.get('/hola', hello_handler2);
 app.post('/users', create_users);
 app.get('/users', devolverUsarios);
+app.post('/user/login', verificarUser);
 app.listen(port, function () {
     console.log(`server running on http://localhost:${port}`);
 });
