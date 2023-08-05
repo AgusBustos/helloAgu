8d5bd5d72cc6f9cf1787b73cef273620e08d95e1
AgusBustos <111938117+AgusBustos@users.noreply.github.com>
2023-08-04 23:22:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function hello_handler(req, res){
 res.send('bye');
 
 };
+
 app.get('/hello', hello_handler);
 app.listen(port, function(){
     console.log(`server running on http://localhost:${port}`);
